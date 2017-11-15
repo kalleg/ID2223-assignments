0bcdeecf4f9be3b7e877984535068799a649ee8d
Kalle Gäfvert <kalle.gafvert@gmail.com>
2017-11-15 11:32:23 +0100

    task 2 done

--- vt17-lab1/src/main/scala/se/kth/spark/lab1/task2/Main.scala
+++ vt17-lab1/src/main/scala/se/kth/spark/lab1/task2/Main.scala
@@ -20,7 +20,7 @@ object Main {
 
     val filePath = "src/main/resources/millionsong.txt"
     val rawDF = sqlContext.read.text(filePath)
-    rawDF.show(5)
+    //rawDF.show(5)
 
     //Step1: tokenize each row
     val regexTokenizer = new RegexTokenizer()
@@ -30,28 +30,28 @@ object Main {
 
     //Step2: transform with tokenizer and show 5 rows
     val tokenized = regexTokenizer.transform(rawDF)
-    tokenized.show(5)
+    //tokenized.show(5)
 
     //Step3: transform array of tokens to a vector of tokens (use our ArrayToVector)
     val arr2Vect = new Array2Vector()
       .setInputCol("row")
       .setOutputCol("vector")
     val vectorized = arr2Vect.transform(tokenized)
-    vectorized.show(5)
+    //vectorized.show(5)
     
     //Step4: extract the label(year) into a new column
     val lSlicer = new VectorSlicer()
       .setInputCol("vector")
       .setOutputCol("year")
     val sliced = lSlicer.setIndices(Array(0)).transform(vectorized)
-    sliced.show(5)
+    //sliced.show(5)
 
     //Step5: convert type of the label from vector to double (use our Vector2Double)
     val v2d = new Vector2DoubleUDF(a => a(0))
       .setInputCol("year")
       .setOutputCol("label")
     val label = v2d.transform(sliced)
-    label.show(5)
+    //label.show(5)
     
     //Step6: shift all labels by the value of minimum label such that the value of the smallest becomes 0 (use our DoubleUDF) 
     label.createOrReplaceTempView("songs")
@@ -59,22 +59,26 @@ object Main {
     val lShifter = new DoubleUDF(a => a - min)
       .setInputCol("label")
       .setOutputCol("labelShift")
-    val labelShift = lShifter.transform(label)
-    labelShift.show(5)
+    //val labelShift = lShifter.transform(label)
+    //labelShift.show(5)
     
     //Step7: extract just the 3 first features in a new vector column
-    //val fSlicer = ???
+    val fSlicer = new VectorSlicer()
+      .setInputCol("vector")
+      .setOutputCol("features")
+    val sliced2 = fSlicer.setIndices(Array(1, 2, 3))
+    //sliced2.transform(labelShift).show(5)
 
     //Step8: put everything together in a pipeline
-    //val pipeline = new Pipeline().setStages(???)
+    val pipeline = new Pipeline().setStages(Array(regexTokenizer, arr2Vect, lSlicer, v2d, lShifter, fSlicer))
 
     //Step9: generate model by fitting the rawDf into the pipeline
-    //val pipelineModel = pipeline.fit(rawDF)
+    val pipelineModel = pipeline.fit(rawDF)
 
     //Step10: transform data with the model - do predictions
-    //???
+    val result = pipelineModel.transform(rawDF)
 
     //Step11: drop all columns from the dataframe other than label and features
-    //???
+    result.select("labelShift", "features").show(5)
   }
 }
